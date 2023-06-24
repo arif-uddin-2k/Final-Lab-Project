a0908ad8387f2d533e41d02ae305d2a6bdf6a002
arif-uddin-2k <123826264+arif-uddin-2k@users.noreply.github.com>
2023-06-24 13:54:56 +0600

    Add files via upload

---
+++ .idea/Diagnosis.java
@@ -0,0 +1,51 @@
+package subModule;
+
+import outModule.Main;
+
+import java.util.ArrayList;
+
+public class Diagnosis {
+    private int diagnosisCode;
+    private String description;
+
+    public static int code = 0;
+    public static ArrayList<Diagnosis> diagnosis = new ArrayList<Diagnosis>();
+
+    public Diagnosis(){
+
+    }
+    public Diagnosis(String description){
+        this.diagnosisCode = code;
+        this.description = description;
+        code++;
+    }
+
+    public static int showDiagnosis(){
+
+        Main.clearScreen();
+        int cnt = 0;
+        System.out.println("\n     Diagnosis list: (DiagnosisCode, DiagnosisTitle)");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------");
+        for(Diagnosis i : diagnosis){
+            cnt++;
+            System.out.println("    |  " + i.getDiagnosisCode()+ "  |  " + i.getDescription() + "  |  ");
+        }
+        return cnt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDiagnosisCode() {
+        return diagnosisCode;
+    }
+
+    public void setDiagnosisCode(int diagnosisCode) {
+        this.diagnosisCode = diagnosisCode;
+    }
+}
